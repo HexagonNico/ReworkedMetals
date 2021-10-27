8b499885c154c905bbfbb0e2580c0c4b65a9ea9b
Nico <nico.hex6@gmail.com>
2021-10-27 14:03:34 +0200

    Furnace animation

--- src/main/java/hexagon/reworkedmetals/block/BlastFurnaceBlock.java
+++ src/main/java/hexagon/reworkedmetals/block/BlastFurnaceBlock.java
@@ -7,7 +7,13 @@ import hexagon.reworkedmetals.registry.ModBlockEntities;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
+import java.util.Random;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -26,5 +32,25 @@ public class BlastFurnaceBlock extends ReworkedFurnaceBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
         return level.isClientSide ? null : createTickerHelper(blockEntityType, ModBlockEntities.BLAST_FURNACE.get(), FurnaceLogic::tickFunction);
+    }
+    
+    @Override
+    public void animateTick(BlockState state, Level level, BlockPos pos, Random random) {
+        if(state.getValue(LIT)) {
+            double x = pos.getX() + 0.5;
+            double y = pos.getY();
+            double z = pos.getZ() + 0.5;
+            if(random.nextDouble() < 0.1) {
+                level.playLocalSound(x, y, z, SoundEvents.BLASTFURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0f, 1.0f, false);
+            }
+            
+            Direction facing = state.getValue(FACING);
+            Direction.Axis axis = facing.getAxis();
+            double d = random.nextDouble() * 0.6 - 0.3;
+            double dx = axis == Direction.Axis.X ? (double) facing.getStepX() * 0.52 : d;
+            double dy = random.nextDouble() * 6.0 / 16.0;
+            double dz = axis == Direction.Axis.Z ? (double) facing.getStepZ() * 0.52 : d;
+            level.addParticle(ParticleTypes.SMOKE, x + dx, y + dy, z + dz, 0.0, 0.0, 0.0);
+        }
     }
 }
