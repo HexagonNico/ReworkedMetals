f2f81fd4d86ea0e4999e8390eb126801792519a2
Nico <nico.hex6@gmail.com>
2021-11-03 14:16:08 +0100

    Crafting recipes and pain

--- src/main/java/hexagon/reworkedmetals/registry/ModRecipes.java
+++ src/main/java/hexagon/reworkedmetals/registry/ModRecipes.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+@SuppressWarnings("unused")
 public class ModRecipes {
     
     public static DeferredRegister<RecipeSerializer<?>> REGISTER = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, ReworkedMetals.ID);
