ce66ae9fdec01da367d377254894bdec5af7c011
Nico <nico.hex6@gmail.com>
2021-11-23 08:20:02 +0100

    JEI Recipe Catalysts

--- src/main/java/hexagonnico/reworkedmetals/integration/jei/JeiPluginClass.java
+++ src/main/java/hexagonnico/reworkedmetals/integration/jei/JeiPluginClass.java
@@ -3,15 +3,18 @@ package hexagonnico.reworkedmetals.integration.jei;
 import hexagonnico.reworkedmetals.ReworkedMetals;
 import hexagonnico.reworkedmetals.content.crafting.ReworkedSmeltingRecipe;
 import hexagonnico.reworkedmetals.content.gui.ReworkedFurnaceScreen;
+import hexagonnico.reworkedmetals.registry.ItemsRegistry;
 
 import java.util.stream.Collectors;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 
@@ -39,6 +42,14 @@ public class JeiPluginClass implements IModPlugin {
 	public void registerGuiHandlers(IGuiHandlerRegistration registration) {
 		registration.addRecipeClickArea(ReworkedFurnaceScreen.class, 81, 26, 24, 17, SmeltingRecipeCategory.ID);
 	}
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ItemsRegistry.SMELTERY.get()), SmeltingRecipeCategory.ID);
+        registration.addRecipeCatalyst(new ItemStack(ItemsRegistry.FURNACE.get()), SmeltingRecipeCategory.ID);
+        registration.addRecipeCatalyst(new ItemStack(ItemsRegistry.BLAST_FURNACE.get()), SmeltingRecipeCategory.ID);
+        registration.addRecipeCatalyst(new ItemStack(ItemsRegistry.KILN.get()), SmeltingRecipeCategory.ID);
+    }
 
     @Override
     public ResourceLocation getPluginUid() {
